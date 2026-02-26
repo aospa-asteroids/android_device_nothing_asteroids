0101f02bacdb573649a916b3570fb77909a6fa20
Saalim Quadri <danascape@gmail.com>
2026-02-26 18:45:57 +0530

    asteroids: Invoke hardware/nothing
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- device.mk
+++ device.mk
@@ -630,6 +630,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    hardware/nothing \
     kernel/nothing/sm7635 \
     kernel/nothing/sm7635-modules
 
